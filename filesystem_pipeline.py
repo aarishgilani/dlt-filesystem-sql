aea4f65cfc911c600ebf9d9ed77e0b2f7db183dc
Aarish <aarishgilani10@gmail.com>
2025-12-26 13:10:14 -0600

    test local file system loading with pgsql

--- filesystem_pipeline.py
+++ filesystem_pipeline.py
@@ -1,9 +1,11 @@
 import dlt
 from dlt.sources.filesystem import filesystem, read_csv
 
-files = filesystem(file_glob="*.csv")
+files = filesystem(bucket_url="./filesystem", file_glob="*.csv")
 reader = (files | read_csv()).with_name("raw_data")
 pipeline = dlt.pipeline(pipeline_name="jaffle_shop_pipeline", dataset_name="jaffle_shop_data", destination="postgres")
 
 info = pipeline.run(reader)
 print(info);
+
+# TODO : add tables for each csv file in the filesystem directory
