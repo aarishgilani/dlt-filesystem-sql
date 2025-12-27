2c178ba875b8b7574f64dd0ad4d42798d29aaeb6
Aarish <aarishgilani10@gmail.com>
2025-12-26 18:57:43 -0600

    Loading strategy--merge when new data is found

--- toy_pipeline.py
+++ toy_pipeline.py
@@ -5,6 +5,7 @@ data = [
     {"id": "1", "name": "bulbasaur", "size": {"weight": 6.9, "height": 0.7}},
     {"id": "4", "name": "charmander", "size": {"weight": 8.5, "height": 0.6}},
     {"id": "25", "name": "pikachu", "size": {"weight": 6, "height": 0.4}},
+    {"id": "39", "name": "jigglypuff", "size": {"weight": 5.5, "height": 0.5}},
 ]
 
 # Set pipeline name, destination, and dataset name
@@ -17,6 +18,6 @@ pipeline = dlt.pipeline(
 # specify table name else the process fails
 # since the table name cannot be inferred from
 # the data structure
-load_info = pipeline.run(data, table_name="pokemon")
+load_info = pipeline.run(data, table_name="pokemon", write_disposition="merge", primary_key=["id"])
 
 print(load_info)
