77cde23cca5c9991717c32de338b251dccbcbbe0
Aarish <aarishgilani10@gmail.com>
2025-12-26 22:09:07 -0600

    init documentation for the project

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'data_load_tool_docs'
+copyright = '2025, Aarish Gilani'
+author = 'Aarish Gilani'
+release = 'v0.0.1-alpha'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
