e3a8b3ae3a9e935644fad78cf56fcaec3d3274f5
Aarish <aarishgilani10@gmail.com>
2025-12-28 10:12:50 -0600

    complete dataframe pipeline using csv from florida state university

--- dataframe_pipeline.py
+++ dataframe_pipeline.py
@@ -7,5 +7,23 @@ A simple DLT pipeline to load data from a pandas DataFrame into a Postgres data 
 import dlt
 import pandas as pd
 
-# TODO : Create a resource function that yields data from a pandas DataFrame 
-@dlt.resource(table_name="dataframe_table")
+@dlt.resource(table_name="dataframe_table", write_disposition="append")
+def fetch_dataframe_data():
+    """
+    Fetches CSV from cloud storage and yields it as a pandas dataframe.
+
+    Yields:
+        pd.DataFrame: A pandas DataFrame containing sample data.
+    """
+    url = "https://people.sc.fsu.edu/~jburkardt/data/csv/hw_200.csv"
+    data = pd.read_csv(url)
+    yield data
+
+pipeline = dlt.pipeline(
+    pipeline_name="dataframe_pipeline",
+    destination="postgres",
+    dataset_name="dataframe_data",
+)
+
+info = pipeline.run(fetch_dataframe_data)
+print(info)
